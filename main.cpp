80b42b463b0a0d57a396867d969c79442367dc2f
Karol01 <kubicki501@gmail.com>
2025-04-21 20:18:22 +0200

    added CLI interface, removed shared folder

--- main.cpp
+++ main.cpp
@@ -1,3 +1,10 @@
 #include "app.hpp"
+#include "logger.hpp"
 
-int main() { Application app{ "script.lang" }; }
+int main(int argc, const char* const* argv) {
+    if(argc > 1) {
+        Application app{ argv[1] };
+    } else {
+        Logger::Warn("Please provide a valid path to a source code file like so: scriptlang path/to/file.lang");
+    }
+}
--- shared/common.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#ifdef LANG_BUILD_AS_DLL
-#define LANG_DLL_FUNC extern "C" __declspec(dllexport)
-#else 
-#define LANG_DLL_FUNC extern "C" __declspec(dllimport)
-#endif
